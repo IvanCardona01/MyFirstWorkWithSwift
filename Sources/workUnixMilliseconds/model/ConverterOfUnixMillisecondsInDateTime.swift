e7e3e0874aacff1d751f10bba61a7fc87c311e4c
IvanCardona <2021ivancardona@gmail.com>
2022-05-11 11:21:35 -0500

    Conversion For Time Zone Incorporated

--- Sources/workUnixMilliseconds/model/ConverterOfUnixMillisecondsInDateTime.swift
+++ Sources/workUnixMilliseconds/model/ConverterOfUnixMillisecondsInDateTime.swift
@@ -1,17 +1,30 @@
-class ConverterOfUnixMillisecondsInDateTime{
-    let staticAgeFromTheUnixTimeInit: Int = 1970
+import Foundation
 
-    public func convertUnixTimeInDateTime(unixTimeInMillisseconds: Int, timeZone: TimeZoneType) -> String{
-        //var diferencyBetwenTimeZoneGeneralAndTimeZoneInput = timeZone
-        var yearsInUnixTime: Int = 0
-        var monthsInUnixTime: Int = 0
-        var daysInUnixTime: Int = 0
-        var hoursInUnixTime: Int = 0
-        var minutsInUnixTime: Int = 0
-        var secondsInUnixTime: Int = 0
-        var timeInTimeZone: Int = 0
-        let dateConverted: String = "\(daysInUnixTime)/\(monthsInUnixTime)/\(yearsInUnixTime) the time in \(timeZone) is: \(timeInTimeZone)"
-        return dateConverted
+class ConverterOfUnixMillisecondsInDateTime{
+    var date: Date?
+    let millisecondsInOneSecond:Double = 1000
+    let countries: Dictionary <String, Double> = [
+                                    "Este de EEUU": -7, 
+                                    "Mexico": 6, 
+                                    "Colombia": -3,
+                                    "España": 0,
+                                    "China": 7
+                                  ]
+
+    public func calculateUnixTimeInDateTimeInTimeZone(unixTimeInMillisseconds: inout Double, diferenceTime: inout Double) -> Date{
+        let unixTimeInSeconds = unixTimeInMillisseconds/millisecondsInOneSecond
+        let secondsContentInOneHour: Double = 3600
+        diferenceTime = diferenceTime*secondsContentInOneHour
+        let epochTime = TimeInterval(unixTimeInSeconds + diferenceTime)
+        date = Date(timeIntervalSince1970: epochTime)
+        return date!;
     }
 
+    public func printCountries() -> String{
+        var listOfCountries: String = "\n"
+        for (country) in countries {
+            listOfCountries += "\(country.key)\n"
+        }
+        return listOfCountries
+    }
 }
